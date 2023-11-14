9bb5d01f92e9538804c915d219e387ee4c51f6e7
arpitgoswami <arpitgoswami@hotmail.com>
2023-11-14 19:52:57 +0530

    Inital Commit

--- src/Flatlaf.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Flatlaf {
-public static void Flatlaf() {
-    Apple JFrame = new JFrame();
-    APPLE.setSize(100, 400);
-}
-}
--- src/SimpleJFrameWithSidebar.java
+++ src/SimpleJFrameWithSidebar.java
@@ -1,87 +1,65 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import javax.swing.*;
+import com.formdev.flatlaf.FlatLightLaf;
 
 public class SimpleJFrameWithSidebar extends JFrame {
 
-    private JTextArea mainTextArea;
+    public static int width = 160;
 
-    public SimpleJFrameWithSidebar() {
-        super("Simple JFrame with Sidebar");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
-
-        // Create components
-        mainTextArea = new JTextArea();
-        mainTextArea.setEditable(false);
-
-        // Create sidebar buttons
-        JButton option1Button = createSidebarButton("Option 1");
-        JButton option2Button = createSidebarButton("Option 2");
-        JButton option3Button = createSidebarButton("Option 3");
-        JButton option4Button = createSidebarButton("Option 4");
-        JButton option5Button = createSidebarButton("Option 5");
-
-        // Create menu bar and menu items
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenuItem exitMenuItem = new JMenuItem("Exit");
-
-        // Add action listener to the exit menu item
-        exitMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-
-        // Add menu items to the menu bar
-        fileMenu.add(exitMenuItem);
-        menuBar.add(fileMenu);
-
-        // Set layout
-        setLayout(new BorderLayout());
-
-        // Add components to the frame
-        setJMenuBar(menuBar);
-
-        JPanel sidebarPanel = new JPanel();
-        sidebarPanel.setLayout(new GridLayout(5, 1));
-        sidebarPanel.add(option1Button);
-        sidebarPanel.add(option2Button);
-        sidebarPanel.add(option3Button);
-        sidebarPanel.add(option4Button);
-        sidebarPanel.add(option5Button);
-
-        add(sidebarPanel, BorderLayout.WEST);
-        add(new JScrollPane(mainTextArea), BorderLayout.CENTER);
-    }
-
-    private JButton createSidebarButton(String buttonText) {
-        JButton button = new JButton(buttonText);
-
-        // Add action listener to the buttons
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayOptionMessage(buttonText);
-            }
-        });
-
-        return button;
-    }
-
-    private void displayOptionMessage(String option) {
-        mainTextArea.setText("Selected Option: " + option);
-    }
-
+    /* Launch the application. */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SimpleJFrameWithSidebar().setVisible(true);
+                try {
+                    Main frame = new Main();
+                    frame.setVisible(true);
+                    frame.setTitle("Management System");
+                    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
+    }
+
+    /* Create the frame. */
+    public SimpleJFrameWithSidebar() throws SQLException {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(width, width, 1200, 800);
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel firstTab = new JPanel(new BorderLayout());
+        tabbedPane.addTab("First Tab", firstTab);
+
+        JPanel secondTab = new JPanel(new GridBagLayout());
+        tabbedPane.addTab("Second Tab", secondTab);
+
+        JPanel thirdTab = new JPanel(new BorderLayout());
+        tabbedPane.addTab("Third Tab", thirdTab);
+
+        JPanel fourthTab = new JPanel(new GridBagLayout());
+        tabbedPane.addTab("Fourth Tab", fourthTab);
+
+        JPanel fifthTab = new JPanel(new BorderLayout());
+        tabbedPane.addTab("Fifth Tab", fifthTab);
+
+        add(tabbedPane, BorderLayout.CENTER);
+
+        JPanel sideBar = new JPanel();
+        sideBar.setPreferredSize(new Dimension(200, 100));
+        sideBar.setBackground(Color.darkGray);
+        tabbedPane.add(sideBar, BorderLayout.EAST);
+
     }
 }
